d6d8fc94f413a468cd8865c72a3d9abf25dc831f
kakon-mehedi <kakon9000@gmail.com>
2022-12-13 12:53:14 +0600

    Basic Todo App Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shortUI/header/header.component';
-import { NavbarComponent } from './shortUI/navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavbarComponent, HomeComponent, AboutComponent, ContactComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
