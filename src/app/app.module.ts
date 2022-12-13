16dc4bdb2733c40d6a4a7c17cc8ab2a8a53ec2f7
kakon-mehedi <kakon9000@gmail.com>
2022-12-13 13:02:37 +0600

    Fresh file deployed to API Branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APIComponent } from './api/api.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, APIComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
