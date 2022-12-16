7c1db540c52c5ccdc8866242adc0d049e276aa99
kakon-mehedi <kakon9000@gmail.com>
2022-12-16 11:38:00 +0600

    Basic UI Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    ModalComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
