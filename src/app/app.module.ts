7ed27cf41f2a2d38cda8e0be686b5ec5b6084864
kakon-mehedi <kakon9000@gmail.com>
2022-12-18 08:13:01 +0600

    BootStrap Added to the Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SingleCategoryComponent } from './single-category/single-category.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CategoriesComponent, SingleCategoryComponent, AddCategoryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
