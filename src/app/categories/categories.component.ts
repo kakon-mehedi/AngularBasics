7e2be001dbe4162a048f63c612da7e5d698203b2
kakon-mehedi <kakon9000@gmail.com>
2022-12-18 10:28:33 +0600

    UI V1

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { ICategory } from '../models/ICategory';
 
+interface user {
+  id: number;
+  name: string;
+  gender: string;
+}
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -10,4 +16,12 @@ export class CategoriesComponent {
   categories: ICategory[] = [
     { id: 1, title: 'Category 1', description: 'Category Description' },
   ];
+
+  showData() {
+    const person1: user = {
+      id: 'kakon',
+      name: 135,
+      gender: 'male',
+    };
+  }
 }
--- src/app/single-category/single-category.component.ts
+++ src/app/single-category/single-category.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ICategory } from '../models/ICategory';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
-  styleUrls: ['./single-category.component.css']
+  styleUrls: ['./single-category.component.css'],
 })
 export class SingleCategoryComponent {
-
+  @Input() category: ICategory | null = null;
 }
