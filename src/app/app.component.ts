b67cd1caf7a19998d5c3993ba1eac8f4b61f8335
kakon-mehedi <kakon9000@gmail.com>
2023-02-19 12:03:48 +0600

    Restore Default code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello Angular';
 
-  showOwnerName = () => {
-    return 'Kakon Mehedi';
-  };
+  showName(name: string) {
+    console.log(name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CompAComponent } from './components/comp-a/comp-a.component';
-import { CompBComponent } from './components/comp-b/comp-b.component';
-import { CompCComponent } from './components/comp-c/comp-c.component';
-import { CompDComponent } from './components/comp-d/comp-d.component';
 
 @NgModule({
-  declarations: [AppComponent, CompAComponent, CompBComponent, CompCComponent, CompDComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
